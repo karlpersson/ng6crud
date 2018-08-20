2a203d8d49198638f61ceaa0d5cc08ae0171a938
Karl Persson <karl.persson@essvision.se>
2018-08-20 13:43:58 +0200

    fix tests

--- src/app/components/edit/edit.component.spec.ts
+++ src/app/components/edit/edit.component.spec.ts
@@ -2,13 +2,26 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EditComponent } from './edit.component';
 
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { RouterModule, Routes } from '@angular/router';
+
+import { APP_BASE_HREF } from '@angular/common';
+
+import { HttpClientModule } from '@angular/common/http';
+
+const routes: Routes = [
+]
+
 describe('EditComponent', () => {
   let component: EditComponent;
   let fixture: ComponentFixture<EditComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EditComponent ]
+      declarations: [ EditComponent ],
+      imports: [ReactiveFormsModule,    RouterModule.forRoot(routes),HttpClientModule],
+      providers: [{provide: APP_BASE_HREF, useValue: '/'}]
     })
     .compileComponents();
   }));
