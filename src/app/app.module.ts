e5cd5bff1601a4f6d7fffcbe83fb8023396d649e
Karl Persson <karl.persson@essvision.se>
2018-08-20 10:29:14 +0200

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CreateComponent } from './components/create/create.component';
+import { IndexComponent } from './components/index/index.component';
+import { EditComponent } from './components/edit/edit.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'create',
+    component: CreateComponent
+  },
+  {
+    path: 'edit/:id',
+    component: EditComponent
+  },
+  {
+    path: 'index',
+    component: IndexComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateComponent,
+    IndexComponent,
+    EditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
+  exports:[RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
